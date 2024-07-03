3a445e6a8bbc082f623c4a920e4e958a13fc3961
MungTsz <mongtaz1030@gmail.com>
2024-07-03 15:19:12 +0800

    create full chain

---
+++ chatbot/modules/full_chain.py
@@ -0,0 +1,34 @@
+from modules.get_chat_model import get_cohere_model
+from langchain.memory import ChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from modules.basic_chain import make_rag_chain
+from modules.memory_chain import create_memory_chain
+
+
+def create_full_chain(
+    model,
+    retriever,
+    qa_system_prompt,
+    contextualize_q_system_prompt,
+    chat_memory=ChatMessageHistory(),
+):
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", qa_system_prompt),
+            # MessagesPlaceholder("chat_history"),
+            ("human", "{question}"),
+        ]
+    )
+
+    rag_chain = make_rag_chain(model, retriever, qa_prompt)
+    full_chain = create_memory_chain(
+        model, rag_chain, chat_memory, contextualize_q_system_prompt
+    )
+    return full_chain
+
+
+def ask_question(chain, query):
+    response = chain.invoke(
+        {"question": query}, config={"configurable": {"session_id": "foo"}}
+    )
+    return response
