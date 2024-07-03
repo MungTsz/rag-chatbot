08725404067d76026507420026e1131b0cae0049
MungTsz <mongtaz1030@gmail.com>
2024-07-03 15:18:29 +0800

    abstract the param

--- chatbot/modules/splitter.py
+++ chatbot/modules/splitter.py
@@ -2,15 +2,17 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document
 from utils.create_logger import get_logger
 
-logger = get_logger()
+# logger = get_logger()
 
 
-def split_documents(docs):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+def split_documents(docs, chunk_size, chunk_overlap):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap
+    )
     contents = docs
     if docs and isinstance(docs[0], Document):
         contents = [doc.page_content for doc in docs]
     chunks = text_splitter.create_documents(contents)
     n_chunks = len(chunks)
-    logger.info(f"Split into {n_chunks} chunks")
+    # logger.info(f"Split into {n_chunks} chunks")
     return chunks
