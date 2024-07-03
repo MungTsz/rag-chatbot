12ab7b7f620d3c71d0f558caa47c57464160f856
MungTsz <mongtaz1030@gmail.com>
2024-07-03 11:38:16 +0800

    add comma

--- chatbot/modules/remote_loader.py
+++ chatbot/modules/remote_loader.py
@@ -4,7 +4,7 @@ from langchain_community.document_loaders import WebBaseLoader
 
 def load_web_page(page_url: str, content_class: str):
     loader = WebBaseLoader(
-        web_paths=(page_url),
+        web_paths=(page_url,),
         bs_kwargs=dict(parse_only=bs4.SoupStrainer(class_=(content_class))),
         # mimic the behavior of a web browser to make the request
         header_template={
